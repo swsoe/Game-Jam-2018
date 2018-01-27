a6370ecfd02819b4fe34da670cd18b323f8fe2ca
MasterKrepta <brianj.pajak@gmail.com>
2018-01-27 11:49:50 -0500

    player select working

---
+++ Game Jam 2018/Assets/_Scripts/PlayerSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour {
+    public GameObject[] selectedPlayer;
+    
+	// Use this for initialization
+	void Awake () {
+
+        GameObject go =  Instantiate(selectedPlayer[PlayerPrefs.GetInt("playerIndex")]) as GameObject;
+        go.transform.parent = this.transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
