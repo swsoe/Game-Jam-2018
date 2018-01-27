8a42081de3aa9b5d17188b4dcd6005799be6cafc
swsoe <npt1@zips.uakron.edu>
2018-01-27 12:18:52 -0500

    asdf

---
+++ Game Jam 2018/Assets/_Scripts/TurretSpawnMaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TurretSpawnMaster : MonoBehaviour, Assets._Scripts.SpawningBase
+{
+
+    List<GameObject> turrets;
+
+    // Use this for initialization
+    void Start()
+    {
+        turrets = new List<GameObject>(GameObject.FindGameObjectsWithTag("TurretSpawn"));
+        IEnumerator spawnFunction = Spawn();
+        StartCoroutine(spawnFunction);
+    }
+
+    public IEnumerator Spawn()
+    {
+        foreach (GameObject T in turrets)
+        {
+            T.GetComponent<TurretSpawn>().Spawn();
+            yield return new WaitForSeconds(0f);
+        }
+
+    }
+}
+
