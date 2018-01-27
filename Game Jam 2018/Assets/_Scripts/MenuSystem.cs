b01cb8bd76b8fbfa558b2cf14f136eef539c5c29
MasterKrepta <brianj.pajak@gmail.com>
2018-01-27 12:24:21 -0500

    player select "done"

--- Game Jam 2018/Assets/_Scripts/MenuSystem.cs
+++ Game Jam 2018/Assets/_Scripts/MenuSystem.cs
@@ -10,19 +10,19 @@ public class MenuSystem : MonoBehaviour {
 
     public void Razer() {
         PlayerPrefs.SetInt("playerIndex", 0);
-        Debug.Log(PlayerPrefs.GetInt("playerIndex"));
+
         SceneManager.LoadScene(0);
     }
 
     public void Galactica() {
         PlayerPrefs.SetInt("playerIndex", 1);
-        Debug.Log(PlayerPrefs.GetInt("playerIndex"));
+
         SceneManager.LoadScene(0);
     }
 
     public void Spearhead() {
         PlayerPrefs.SetInt("playerIndex", 2);
-        Debug.Log(PlayerPrefs.GetInt("playerIndex"));
+
         SceneManager.LoadScene(0);
     }
 }
