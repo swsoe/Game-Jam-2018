228f604db7f6d70238162ad2c6d035b5cd3b0681
MasterKrepta <brianj.pajak@gmail.com>
2018-01-27 13:25:54 -0500

    Title screen tweaks, and prefab tweaks
    base speed changed to each ship, tied to the play area movement script

--- Game Jam 2018/Assets/brians base code/PlayerScripts/PlayerMovement.cs
+++ Game Jam 2018/Assets/brians base code/PlayerScripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     
     public float speed = 1.0f;
- 
 
+    public float startingSpeed = 0.5f;
     // Update is called once per frame
 
     private void Start() {
